46a4cc5f2f52fddc945df8a70416d2564feed3aa
hellvita <seredaolga03@gmail.com>
2026-02-15 00:11:17 +0200

    add the getUser and updateUser controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,28 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
+export const getUser = async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  res.status(200).json(user);
+};
+
+export const updateUser = async (req, res) => {
+  const user = await User.findOneAndUpdate({ _id: req.user._id }, req.body, {
+    new: true,
+  });
+
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  res.status(200).json(user);
+};
+
 export const updateUserAvatar = async (req, res) => {
   const { file } = req;
   if (!file) {
