22d9538130600aa20fbd4a24ea076067728041ad
hellvita <seredaolga03@gmail.com>
2026-02-14 18:25:58 +0200

    add the basic middlewares (logger, notFoundHandler, errorHandler)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
+import 'dotenv/config';
+
 import express from 'express';
+import cors from 'cors';
+import { logger } from './middleware/logger.js';
+
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 const PORT = Number.parseInt(process.env.PORT) || 3000;
+
+app.use(logger);
+app.use(cors());
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, (error) => {
   if (error) {
