e53f722838d74aef55981c6a8017da226a4d540b
hellvita <seredaolga03@gmail.com>
2026-02-15 18:09:58 +0200

    update the user validation schema

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -4,5 +4,6 @@ export const updateUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().lowercase(),
     password: Joi.string().min(8),
+    username: Joi.string().min(1).max(40),
   }),
 };
