7c44a5ae42eb7df7a42bb8ac900fa33a8f5487d9
hellvita <seredaolga03@gmail.com>
2026-02-16 22:20:50 +0200

    update the register user validation schema

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -3,7 +3,7 @@ import { Joi, Segments } from 'celebrate';
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required().lowercase(),
-    password: Joi.string().min(8).required(),
+    password: Joi.string().min(6).required(),
   }),
 };
 
