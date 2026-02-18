08d95357d358b4849b48e2c26e4b3bc2a6d478d9
hellvita <seredaolga03@gmail.com>
2026-02-18 17:20:35 +0200

    update the validation schema for the user update request

--- src/validations/userValidation.js
+++ src/validations/userValidation.js
@@ -3,7 +3,7 @@ import { Joi, Segments } from 'celebrate';
 export const updateUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().lowercase(),
-    password: Joi.string().min(8),
+    password: Joi.string().min(6),
     username: Joi.string().min(1).max(40),
     avatar: Joi.string(),
   }),
